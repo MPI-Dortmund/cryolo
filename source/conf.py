43a17edc5572ac3ea13bac08f2014a5b8b6c34a9
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2020-11-03 09:37:23 +0100

    latest warning

--- source/conf.py
+++ source/conf.py
@@ -30,6 +30,10 @@ author = 'Thorsten Wagner'
 extensions = [
     'versionwarning.extension',
 ]
+versionwarning_messages = {
+    'latest': 'This is the documentation for an unreleased development version. Documentation is '
+              'available for the current stable release, or for older versions, through the "v:" menu at the bottom left.',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
