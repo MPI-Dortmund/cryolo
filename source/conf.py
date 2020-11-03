f43c83d44bed8eb516579149195065a9ecd49af5
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2020-11-03 09:32:10 +0100

    add version warning extension

--- source/conf.py
+++ source/conf.py
@@ -28,6 +28,7 @@ author = 'Thorsten Wagner'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'versionwarning.extension',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
