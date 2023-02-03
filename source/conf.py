1837d243e99f70b473cb99bf6f774fb25d933e5b
thorstenwagner <twa1@posteo.de>
2023-02-03 11:49:45 +0100

    add sphinx rtd theme

--- source/conf.py
+++ source/conf.py
@@ -14,6 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
 # -- Project information -----------------------------------------------------
 
 project = 'crYOLO'
